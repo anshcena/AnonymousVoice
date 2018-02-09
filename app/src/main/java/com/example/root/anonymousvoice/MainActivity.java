09ddbb8e7d44ff30e62dac19ee59a3054dec9b23
anshcena <anshcena.1996@gmail.com>
2018-02-09 20:41:55 +0000

    Merge branch 'master' of https://github.com/anshcena/AnonymousVoice
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/root/anonymousvoice/MainActivity.java
+++ app/src/main/java/com/example/root/anonymousvoice/MainActivity.java
@@ -3,39 +3,46 @@ package com.example.root.anonymousvoice;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompatSideChannelService;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
 
     Button generatetoken,exit;
     ImageButton facebookb,twitterb;
     RadioGroup r;
-    RadioButton rfb,rtw,rstatus,s;
+    private RadioButton rfb,rtw,rstatus;
 
-    String temp; //for radiobutton text
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //linking of buttons
         facebookb = (ImageButton) findViewById(R.id.fbbutton);
         twitterb = (ImageButton) findViewById(R.id.twbutton);
         generatetoken = (Button) findViewById(R.id.status);
         exit = (Button) findViewById(R.id.exit);
-         r = (RadioGroup) findViewById(R.id.radio);
+        r = (RadioGroup) findViewById(R.id.radio);
         rfb = (RadioButton) findViewById(R.id.gofb);
         rtw = (RadioButton) findViewById(R.id.gotw);
         rstatus = (RadioButton) findViewById(R.id.checkstatus);
+
+
+
+
 
 
         // fb click button
@@ -61,38 +68,38 @@ public class MainActivity extends AppCompatActivity {
 
         //generate tokens
 
+
         generatetoken.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Dialog d = new Dialog(MainActivity.this);
                 d.setContentView(R.layout.dailogboxforchecktokenstatus);
-                d.setTitle("SELECT");
-            /*    int select;
-                select=r.getCheckedRadioButtonId();
-                s=(RadioButton)findViewById(select);
-                s.getText().toString();
-                temp=s.getText().toString();
-                if(temp=="GO TO FACEBOOK PAGE")
-                {
-                    Intent i=new Intent(getApplicationContext(),Facebookpage.class);
-                    startActivity(i);
+                d.setTitle("CHOOSE");
+
+
+
+                public void onRadioButtonClicked(View view) {
+                    // Is the button now checked
+                    boolean checked = ((RadioButton) view).isChecked();
+
+                    // Check which radio button was clicked
+                    switch(view.getId()) {
+                        case R.id.gofb:
+                            if (checked)
+
+                                break;
+                        case R.id.gotw:
+                            if (checked)
+
+                                break;
+                    }
                 }
-                else if(temp=="GO TO TWITTER ACCOUNT")
-                {
-                    Intent i=new Intent(getApplicationContext(),TwitterActivity.class);
-                    startActivity(i);
-                }
-                else
-                {
-                    Intent i=new Intent(getApplicationContext(),TokenStatus.class);
-                    startActivity(i);
-                }*/
+
 
                 d.show();
 
             }
         });
-
 
 
 
@@ -124,6 +131,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.gofb:
+                break;
+            case R.id.gotw:
+                break;
+            case R.id.checkstatus:
+                break;
+
+        }
 
     }
 }
