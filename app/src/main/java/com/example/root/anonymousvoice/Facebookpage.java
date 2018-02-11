95f613d8c98dca0631cf8f539f3223d2028d8ef4
anshcena <anshcena.1996@gmail.com>
2018-02-11 20:46:11 +0000

    Merge branch 'master' of https://github.com/anshcena/AnonymousVoice
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/root/anonymousvoice/Facebookpage.java
+++ app/src/main/java/com/example/root/anonymousvoice/Facebookpage.java
@@ -2,12 +2,25 @@ package com.example.root.anonymousvoice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Facebookpage extends AppCompatActivity {
+
+    WebView fblink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebookpage);
+        fblink=(WebView)findViewById(R.id.fbweb);
+        fblink.getSettings().setLoadsImagesAutomatically(true);
+        fblink.getSettings().setJavaScriptEnabled(true);
+        fblink.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        fblink.setWebViewClient(new WebViewClient());
+        fblink.loadUrl("http://www.google.com");
+
+
     }
 }
