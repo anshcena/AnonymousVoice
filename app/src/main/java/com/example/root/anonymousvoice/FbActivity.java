ea18f5682cd3c882d5b7e0bd3f87a2bfc9abd4b6
anshcena <anshcena.1996@gmail.com>
2018-02-20 11:29:13 +0000

    getting image file imagefilepath.java new added

--- app/src/main/java/com/example/root/anonymousvoice/FbActivity.java
+++ app/src/main/java/com/example/root/anonymousvoice/FbActivity.java
@@ -1,6 +1,15 @@
 package com.example.root.anonymousvoice;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,50 +19,66 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicNameValuePair;
 
 import static android.os.Build.VERSION_CODES.O;
 
-public class FbActivity extends AppCompatActivity {
+public class FbActivity extends AppCompatActivity  {
 
     EditText description;
     Spinner category;
     ImageButton attachment;
     Button Postfacebook;
-  public static final int Pick_Image=1;
+    TextView textFile;
+    private static final int MY_INTENT_CLICK=302;
+    String des = "";
+    String inputdata;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fb);
 
         //Linking variables to id
-
-        description=(EditText)findViewById(R.id.description);
-        category=(Spinner)findViewById(R.id.category);
-        attachment=(ImageButton)findViewById(R.id.attachmen);
-        Postfacebook=(Button)findViewById(R.id.postFB);
-
+        description = (EditText) findViewById(R.id.description);
+        category = (Spinner) findViewById(R.id.category);
+        attachment = (ImageButton) findViewById(R.id.attachmen);
+        Postfacebook = (Button) findViewById(R.id.postFB);
+       textFile=(TextView)findViewById(R.id.textfile);
         //Setting click actions
 
         description.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                    String inputdata;
-                    inputdata=description.getText().toString();
+
+                inputdata = description.getText().toString();
 
             }
         });
@@ -61,32 +86,41 @@ public class FbActivity extends AppCompatActivity {
 
         //Spinner
         // making adapter view
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.categories, android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         category.setAdapter(adapter);
 
-                 category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                        Toast.makeText(FbActivity.this, " CATEGORY : "+ String.valueOf(category.getSelectedItem()), Toast.LENGTH_SHORT).show();
-                    }
 
-                    @Override
-                    public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(FbActivity.this, " CATEGORY : " + String.valueOf(category.getSelectedItem()), Toast.LENGTH_SHORT).show();
 
-                    }
-                });
 
-            //attachments
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+
+            }
+        });
+
+        //attachments
         attachment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent=new Intent();
+
+                Intent intent = new Intent();
+
                 intent.setType("image/*");
+
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-              startActivityForResult(Intent.createChooser(intent, "Select Picture"), Pick_Image);
+
+                startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), MY_INTENT_CLICK);
+
 
             }
         });
@@ -97,52 +131,41 @@ public class FbActivity extends AppCompatActivity {
         Postfacebook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String  des = description.getText().toString();
+                
 
-                //check whether the des empty or not
-                if(des.length()>0 || category.getSelectedItem().toString() != "-----Blank-----") {
-                    HttpClient httpclient = new DefaultHttpClient();
-                    HttpPost httppost = new HttpPost("http://www.google.com/");
 
-                    try {
-                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-                        //nameValuePairs.add(new BasicNameValuePair("id", "01"));
-                        nameValuePairs.add(new BasicNameValuePair("description", des));
-                        nameValuePairs.add(new BasicNameValuePair("category", category.getSelectedItem().toString()));
-                        nameValuePairs.add(new BasicNameValuePair("media", attachment.toString()));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                        httpclient.execute(httppost);
-                        //description.setText(""); //reset the message text field
-                        Toast.makeText(getBaseContext(),"Sent message to server",Toast.LENGTH_SHORT).show();
-                    } catch (ClientProtocolException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    //display message if text field is empty
-                    Toast.makeText(getBaseContext(),"All fields are required",Toast.LENGTH_SHORT).show();
-                }
             }
         });
 
 
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
 
 
+            if(resultCode == Activity.RESULT_OK && data != null)
+                {
+                    if (requestCode == MY_INTENT_CLICK)
+                    {
 
 
+                        String selectedImagePath;
+                        Uri selectedImageUri = data.getData();
+
+                        //MEDIA GALLERY
+                        selectedImagePath = ImageFilePath.getPath(getApplicationContext(), selectedImageUri);
+                        Log.i("Image File Path", ""+selectedImagePath);
 
 
-
-
-
-
-
-
-
-
-
-
-
+                        textFile.setText("File selected : \n"+selectedImagePath);
+                    }
+                }
+            }
         }
-}
+    }
+
+
+
+
+
